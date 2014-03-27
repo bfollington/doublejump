ab6e576d20c308c765d8ddf4cd2ec8f5ef33cc12
Ben Follington <bfollington@gmail.com>
2014-03-27 19:41:59 +1000

    Markdown editing and parsing for step content

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,6 +11,40 @@ $( function() {
 
     });
 
+    if (EpicEditor) {
+
+        var opts = {
+          container: 'epiceditor',
+          textarea: 'epiceditor-target',
+          basePath: '/javascripts/epiceditor/',
+          clientSideStorage: true,
+          localStorageName: 'epiceditor',
+          useNativeFullscreen: true,
+          parser: marked,
+          file: {
+            name: 'epiceditor',
+            defaultContent: '',
+            autoSave: 100
+          },
+          theme: {
+            base: 'themes/base/epiceditor.css',
+            preview: 'themes/preview/preview-light.css',
+            editor: 'themes/editor/epic-dark.css'
+          },
+          focusOnLoad: false,
+          string: {
+            togglePreview: 'Toggle Preview Mode',
+            toggleEdit: 'Toggle Edit Mode',
+            toggleFullscreen: 'Enter Fullscreen'
+          },
+          autogrow: false
+        }
+
+        var editor = new EpicEditor(opts).load();
+
+        $("#epiceditor-target").parent().hide();
+
+    }
 
 });
 
