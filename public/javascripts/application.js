b4766a5ab28a04e79d6c2fb0023b9bef8df4f0c3
Ben Follington <bfollington@gmail.com>
2014-04-10 13:30:10 +1000

    More updating layouts for boostrap

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,10 +2,7 @@ $( function() {
 
     FastClick.attach(document.body);
 
-    $('[rel=tooltip]').tooltip();
-    $('[rel=popover]').popover();
-    $('[rel=popover]').click( function (e) { e.preventDefault(); } );
-    $('[rel=popover][data-trigger=hover]').off("click");
+    boostrapMods();
 
     bindSlugFields();
 
@@ -17,6 +14,33 @@ $( function() {
 
     bindProgressBarResize();
 });
+
+
+
+
+
+/**
+ * %BOOTSTRAP MODS
+ */
+
+
+function boostrapMods()
+{
+    $('[rel=tooltip]').tooltip();
+    $('[rel=popover]').popover();
+    $('[rel=popover]').click( function (e) { e.preventDefault(); } );
+    $('[rel=popover][data-trigger=hover]').off("click");
+
+    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+    $('.dropdown').on('show.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(100);
+    });
+
+    // ADD SLIDEUP ANIMATION TO DROPDOWN //
+    $('.dropdown').on('hide.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(100);
+    });
+}
 
 
 
