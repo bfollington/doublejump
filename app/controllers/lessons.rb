11ce5fc33a21c42d788170d3453732f8407b903f
Ben Follington <bfollington@gmail.com>
2014-04-17 20:00:25 +1000

    Improved form layout

--- app/controllers/lessons.rb
+++ app/controllers/lessons.rb
@@ -2,13 +2,40 @@ LearnToGameDev::App.controllers :lessons do
     
   layout :app
 
-  get :new do
+  get :make do
     new_lesson
   end
 
-  post :new do
+  post :make do
 
     @lesson = Lesson.create( :title => params[:lesson][:title], :description => params[:lesson][:description], :slug => params[:lesson][:slug] )
+
+    params[:lesson][:steps].each do |step_id|
+      @lesson.steps.push( Step.find(step_id) )
+    end
+
+    if @lesson.valid?
+      @lesson.save
+      render 'lessons/new_success'
+    else
+      new_lesson
+    end
+
+  end
+
+  get :make, :with => :slug do
+    @lesson = Lesson.where( :slug => params[:slug]).first
+    new_lesson
+  end
+
+  post :make, :with => :slug do
+
+    @lesson = Lesson.where( :slug => params[:slug]).first
+
+    @lesson.title = params[:lesson][:title]
+    @lesson.slug = params[:lesson][:slug]
+    @lesson.description = params[:lesson][:description]
+    @lesson.steps = []
 
     params[:lesson][:steps].each do |step_id|
       @lesson.steps.push( Step.find(step_id) )
