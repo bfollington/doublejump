be8e690007c3959af3456a9ad036bb9586de8fac
Ben Follington <bfollington@gmail.com>
2014-04-18 13:53:22 +1000

    Error handling for invalid courses and lessons etc

--- app/controllers/learn.rb
+++ app/controllers/learn.rb
@@ -8,26 +8,78 @@ LearnToGameDev::App.controllers :learn do
 
   get :view_course, :map => '/learn/:course/' do
     
-    @course = Course.where(:slug => params[:course]).first
+    fetch_course(params[:course])
 
     render 'learn/course'
   end
 
   get :view_lesson, :map => '/learn/:course/:lesson/' do
 
-    @course = Course.where(:slug => params[:course]).first
-    @lesson = Lesson.where(:slug => params[:lesson]).first
+    fetch_course(params[:course])
+    fetch_lesson(params[:lesson])
+
+    if @course.nil? || @lesson.nil?
+      halt 404
+    end
 
     render 'learn/lesson'
   end
 
+  get :finish_lesson, :map => '/learn/:course/:lesson/finish' do
+    
+    fetch_course(params[:course])
+    fetch_lesson(params[:lesson])
+
+    render 'learn/finish_lesson'
+  end
+
   get :view_step, :map => '/learn/:course/:lesson/:step' do
     
-    @course = Course.where(:slug => params[:course]).first
-    @lesson = Lesson.where(:slug => params[:lesson]).first
-    @step = Step.where(:slug => params[:step]).first
+    fetch_course(params[:course])
+    fetch_lesson(params[:lesson])
+    fetch_step(params[:step])
+
+    if @course.nil? || @lesson.nil?
+      halt 404
+    end
 
     render 'learn/step'
   end
 
+end
+
+
+
+
+
+
+
+def fetch_course(slug)
+  @course = Course.where(:slug => slug).first
+
+  if @course.nil?
+    halt 404
+  end
+
+  return @course
+end
+
+def fetch_lesson(slug)
+  @lesson = Lesson.where(:slug => slug).first
+
+  if @lesson.nil?
+    halt 404
+  end
+
+  return @course
+end
+
+def fetch_step(slug)
+  @step = Step.where(:slug => slug).first
+
+  if @step.nil?
+    halt 404
+  end
+
+  return @step
 end
