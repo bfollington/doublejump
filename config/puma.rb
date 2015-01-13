d1f7490ae65c5ee8ab0355beab7820fae0dffec9
Ben Follington <bfollington@gmail.com>
2015-01-13 15:49:04 +1000

    Puma as daemon

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,12 @@
 root = "#{Dir.getwd}"
- 
+
 bind "unix://#{root}/tmp/puma/socket"
 pidfile "#{root}/tmp/puma/pid"
 state_path "#{root}/tmp/puma/state"
 rackup "#{root}/config.ru"
- 
+
 threads 8, 16
-preload_app! 
+preload_app!
+daemonize
 
 activate_control_app
