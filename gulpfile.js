14a7057434d4ae80e005202c5a6323d832909628
Ben Follington <bfollington@gmail.com>
2015-02-11 16:30:05 +1000

    Enhance the gulpfile a bit, no longer crash on errors and also added ability to boot server

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,51 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
+    gutil = require('gulp-util'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     clean = require('gulp-clean'),
+    shell = require('gulp-shell'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload');
 
-gulp.task('scripts', function() {
-  return gulp.src('javascript/**/*.js')
+gulp.task('scripts', buildJs);
+
+function buildJs()
+{
+    return gulp.src('javascript/**/*.js')
     .pipe(concat('doublejump.js'))
     .pipe(gulp.dest('public/javascripts'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
+    .pipe(uglify().on('error', ignoreError))
     .pipe(gulp.dest('public/javascripts'));
-});
+}
 
-gulp.task('watch', function() {
+gulp.task('watch', ['scripts'], function() {
 
   // Watch .js files
   gulp.watch('javascript/**/*.js', ['scripts']);
-
 });
+
+function ignoreError(e)
+{
+    gutil.log(e);
+    this.emit('end');
+}
+
+gulp.task('server', ['watch'], function() {
+
+    gulp.src('').pipe( shell(['mongod &']) );
+    gulp.src('').pipe( shell(['padrino s']) );
+});
+
+gulp.task('down', function() {
+    gulp.src('').pipe( shell(['killall mongod']) );
+});
+
+gulp.task('deploy', function() {
+    gulp.src('').pipe( shell(['padrino rake deploy']) );
+});
+
