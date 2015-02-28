b58e34919eb7544cecf2d8b201889f2a9f6da6a3
Ben Follington <bfollington@gmail.com>
2015-02-28 17:44:45 +1000

    Don't include the view model name in the model

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -1395,8 +1395,12 @@ require("ui/js/_comment_icon_view");
 function run()
 {
     $("[data-view-model]").each( function() {
-        console.log( $(this).attr("data-view-model") );
-        new (require( $(this).attr("data-view-model") ))({el: $(this), model: new Backbone.Model($(this).data()) });
+        var vm = $(this).attr("data-view-model");
+        $(this).removeAttr("data-view-model");
+
+        new (require(vm))({el: $(this), model: new Backbone.Model($(this).data()) });
+
+
     });
 }
 
