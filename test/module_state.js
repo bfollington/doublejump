ff290b64af31c3efad85790ef24a69806daa6976
Ben Follington <bfollington@gmail.com>
2015-08-30 18:12:55 +1000

    Test updates

--- test/module_state.js
+++ test/module_state.js
@@ -64,6 +64,20 @@ describe('Module State', function() {
         expect(store.getState().module.items[OTHER_SAMPLE_MODULE._id.$oid].data.name).toBe(OTHER_SAMPLE_MODULE.name);
     });
 
+    it('RECEIVE_MODULES with several at a time', function() {
+        var mod = require.requireActual('actions/Module');
+        var store = getStore(redux);
+
+        store.dispatch(mod.receiveModules([
+            OTHER_SAMPLE_MODULE,
+            SAMPLE_MODULE
+        ]));
+
+        expect(store.getState().module.areFetching).toBeFalsy();
+        expect(store.getState().module.items[OTHER_SAMPLE_MODULE._id.$oid].data.name).toBe(OTHER_SAMPLE_MODULE.name);
+        expect(store.getState().module.items[SAMPLE_MODULE._id.$oid].data.name).toBe(SAMPLE_MODULE.name);
+    });
+
     it('RECEIVE_MODULES and preserves state', function() {
         var mod = require.requireActual('actions/Module');
         var store = getStore(redux);
@@ -79,8 +93,6 @@ describe('Module State', function() {
         store.dispatch(mod.receiveModules([
             SAMPLE_MODULE
         ]));
-
-        console.log(store.getState().module);
 
         expect(store.getState().module.items[SAMPLE_MODULE._id.$oid].data.name).toBe(SAMPLE_MODULE.name);
         expect(store.getState().module.items[OTHER_SAMPLE_MODULE._id.$oid].data.name).toBe(OTHER_SAMPLE_MODULE.name);
