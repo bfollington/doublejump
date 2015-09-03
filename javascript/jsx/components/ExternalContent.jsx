110482c25150510dc406b93bc89f8240016c5502
Ben Follington <bfollington@gmail.com>
2015-09-03 15:54:11 +1000

    Use iframe if possible for external contentt

--- javascript/jsx/components/ExternalContent.jsx
+++ javascript/jsx/components/ExternalContent.jsx
@@ -6,6 +6,12 @@ import { Button } from "components/input/Input.jsx";
 export class ExternalContent extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            iframeDisplay: "none",
+            failedToLoad: false,
+            loading: true
+        };
     }
 
     onClick() {
@@ -13,14 +19,51 @@ export class ExternalContent extends React.Component {
         win.focus();
     }
 
+    onLoadExternal() {
+        console.log('DID LAOD');
+
+        if (new Date().getTime() - this.startLoadingFrameTime < 1024) {
+            console.log("BOY THAT'S A FAST LOAD");
+            this.setState({
+                failedToLoad: true,
+                loading: false
+            });
+        } else {
+            this.setState({
+                iframeDisplay: "block",
+                failedToLoad: false,
+                loading: false
+            });
+        }
+
+
+    }
+
     render() {
+
+        if (!this.hasRendered) {
+            this.hasRendered = true;
+
+            this.startLoadingFrameTime = new Date().getTime();
+        }
+
         return (
             <div className="ExternalContent">
-                <MessageFromUs>This concept is hosted on an external website, click the button below when you're ready.</MessageFromUs>
-                <p>You'll be heading to <i>{this.props.module.url}</i></p>
-                <div className="text-center">
-                    <Button text="Open Resource" onClick={this.onClick.bind(this)} />
-                </div>
+                <MessageFromUs>This concept is hosted on an external website at <i>{this.props.module.url}</i>.</MessageFromUs>
+
+                { this.state.loading ? "Loading..." : null }
+                {
+                    this.state.failedToLoad ?
+
+                        <div className="text-center">
+                            <Button text="Open Resource" onClick={this.onClick.bind(this)} />
+                        </div>
+                    :
+                        <iframe style={{display: this.state.iframeDisplay}} src={this.props.module.url} onLoad={this.onLoadExternal.bind(this)} />
+                }
+
+
+
             </div>
         );
     }
